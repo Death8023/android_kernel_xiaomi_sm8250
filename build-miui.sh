ed2e1814e360c42c737504b3627454f0851758db
Death8023 <93075648+Death8023@users.noreply.github.com>
2026-05-19 18:18:10 +0800

    Update build-miui.sh

--- build-miui.sh
+++ build-miui.sh
@@ -289,7 +289,7 @@ echo "Build for MIUI finished."
 
 cd anykernel 
 
-ZIP_FILENAME=APTKernel_MIUI_${TARGET_DEVICE}_${KSU_ZIP_STR}_$(date +'%Y%m%d_%H%M%S')_anykernel3_${GIT_COMMIT_ID}.zip
+ZIP_FILENAME=Alioth_perf_Kernel_MIUI_${TARGET_DEVICE}_${KSU_ZIP_STR}_$(date +'%Y%m%d_%H%M%S')_anykernel3_${GIT_COMMIT_ID}.zip
 
 zip -r9 $ZIP_FILENAME ./* -x .git .gitignore out/ ./*.zip
 
